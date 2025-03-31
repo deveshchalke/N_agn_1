9410e81284c887ccb782dfaf5f70f08f6eb74ae9
Devesh <deveshchalke07@gmail.com>
2025-03-31 01:49:27 -0400

    Initial project setup

---
+++ Proxy.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+import os
+import argparse
+import re
+import time
+
+BUFFER_SIZE = 1000000
+
+def main():
+    ###################################################################
+    # Proxy server started
+    ###################################################################
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hostname', help='the IP Address Of Proxy Server')
+    parser.add_argument('port', help='the port number of the proxy server')
+    args = parser.parse_args()
+    proxyHost = args.hostname
+    proxyPort = int(args.port)
+
+    # Socket setup
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.bind((proxyHost, proxyPort))
+        serverSocket.listen(50)
+        print('Proxy server started')
+    except Exception as e:
+        print('Server startup failed:', e)
+        sys.exit()
+
